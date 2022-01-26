86da77dbd2e55788658db19ffaaace0d3b02f1fd
Michelle Fortuin <38165467+michelle9193@users.noreply.github.com>
2022-01-26 19:47:23 +0200

    Update ex_03_03.py
    The problem:
    Kept on receiving the except() value when adding an input.
    
    The solution:
    Create var after score to float() the input because the value was seen as a string.

--- Exercises/ex_3/ex_03_03.py
+++ Exercises/ex_3/ex_03_03.py
@@ -1,14 +1,18 @@
+# Exercise 3: Write a program to prompt for a score between 0.0 and 1.0. If the score is out of range,
+# print an error message. If the score is between 0.0 and 1.0, print a grade using the following table:
+
 score = input('Enter a score between 0.0 and 1.0 ')
+s = float(score)
 try:
-    if score >= 0.9:
-        print('A');
-    if score >= 0.8:
-        print('B');
-    if score >= 0.7:
-        print('C');
-    if score >= 0.6:
-        print('D');
-    if score < 0.6:
-        print('You have Failed');
+    if s >= 0.9:
+        print('A')
+    elif s >= 0.8:
+        print('B')
+    elif s >= 0.7:
+        print('C')
+    elif s >= 0.6:
+        print('D')
+    else:
+        print('You have Failed')
 except:
     print('Please check the number you have printed ')
